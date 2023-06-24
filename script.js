8aaf5c6ce3201557aa0fbcf4d28adef225624b6b
Dev <devrai197551@gmail.com>
2023-06-24 21:03:25 +0530

    js logic built and implimented

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const Grid=document.querySelector('.Grid')
+const colors=document.querySelectorAll('.colors')
+let Selected_color;
+colors.forEach((col)=>{
+    col.addEventListener('click',()=>{
+       console.log(col.id)
+    })
+    })
 for (let i = 0; i < 60; i++) {
     const grid = document.createElement('div');
     grid.style.border = '2px solid white';
@@ -7,6 +14,7 @@ for (let i = 0; i < 60; i++) {
     grid.style.transition = 'all 0.35s linear';
     Grid.appendChild(grid);
     grid.addEventListener("mouseenter",()=>{
-        grid.style.background='purple';
+        grid.style.background= `${Selected_color}`;
     })
-  }
+}
+
